91cec1eddaf2442f6fefd3257830069589bba40c
Utkarsh Pathak <utkarsh.pathak@bhnetwork.com>
2021-06-15 01:34:17 +0530

    try commenting out lifecycle

--- ecs.tf
+++ ecs.tf
@@ -11,9 +11,9 @@ resource "aws_launch_configuration" "ecs-example-launchconfig" {
   iam_instance_profile = aws_iam_instance_profile.ecs-ec2-role.id
   security_groups      = [aws_security_group.ecs-securitygroup.id]
   user_data            = "#!/bin/bash\necho 'ECS_CLUSTER=example-cluster' > /etc/ecs/ecs.config\nstart ecs"
-  lifecycle {
-    create_before_destroy = true
-  }
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
 }
 
 resource "aws_autoscaling_group" "ecs-example-autoscaling" {
